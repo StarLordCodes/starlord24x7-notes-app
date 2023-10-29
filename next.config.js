3f0cc238eb96a366445bd2097ab6ccdcee110133
Abhijith Mammoottil <amammoottil@outlook.com>
2023-10-29 08:29:14 +0530

    redirecting path

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    async rewrites() {
+        return [
+          {
+            source: '/notes*',
+            destination: '/notes',
+          },
+        ]
+      },
+}
 
 module.exports = nextConfig
