3110c39a0bc60704047441411ccaff03c4a9f20c
Abhijith Mammoottil <amammoottil@outlook.com>
2023-10-29 08:52:28 +0530

    domain name change

--- app/passport.js
+++ app/passport.js
@@ -6,8 +6,8 @@ const passportInstance = new passport.Passport({
     environment: config.Environment.SANDBOX,
   }),
   clientId: "S6zlYWF3tBgGppBmFlvlnsLyJb0x0ugZ",
-  redirectUri: "https://starlord24x7-simple-notes.vercel.app/notes",
-  logoutRedirectUri: "https://starlord24x7-simple-notes.vercel.app",
+  redirectUri: "https://starlord24x7-notes-app.vercel.app/notes",
+  logoutRedirectUri: "https://starlord24x7-notes-app.vercel.app",
   audience: "platform_api",
   scope: "openid offline_access email transact",
 });
