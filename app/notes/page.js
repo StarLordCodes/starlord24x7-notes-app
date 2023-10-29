129c60591d51c425181c64bb4b05c451b9383da1
Abhijith Mammoottil <amammoottil@outlook.com>
2023-10-29 09:39:52 +0530

    trying to change passportinstance to passport

--- app/notes/page.js
+++ app/notes/page.js
@@ -1,20 +1,20 @@
-'use client'
+"use client";
 
 import Header from "@/components/Header";
 import Notes from "@/components/Notes";
 import { useEffect } from "react";
-import passportInstance from "../passport";
+import { passport } from "@imtbl/sdk";
 
 export default function NotesPage() {
-    useEffect(()=>{
-        window.addEventListener('load', function() {
-          passportInstance.loginCallback();
-        });
-      },[]);
-  return(
-  <div>
-    <Header />
-    <Notes />
-  </div>
-  )
+  useEffect(() => {
+    window.addEventListener("load", function () {
+      passport.loginCallback();
+    });
+  }, []);
+  return (
+    <div>
+      <Header />
+      <Notes />
+    </div>
+  );
 }
